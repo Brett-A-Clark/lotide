c0de18770eca34436f5cdcf389ba5fbd32d03963
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-26 21:40:09 +0000

    Created test for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,11 @@
+const middle = require('../middle.js');
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+
+// TEST CODE
+middle([1, 2, 3, 4, 5]);
+middle([1, 2, 3, 4, 5, 6]);
+
+
+
+
