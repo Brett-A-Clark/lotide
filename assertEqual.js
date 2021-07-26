a3dcb3539b82c1191882f0d56374ed86299cfd25
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-26 19:17:59 +0000

    Added import/export capability to file.

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,10 @@ const assertEqual = function(actual, expected) {
   const fact1 = actual;
   const fact2 = expected;
   if (actual === expected) {
-    console.log(`Assertion Passed: ${fact1} === ${fact2}`);
+    console.log(`❌ ❌ ❌ Assertion Passed: ${fact1} === ${fact2}`);
   } else {
-    console.log(`Assertion Failed: ${fact1} !== ${fact2}`);
+    console.log(`✅ ✅ ✅ Assertion Failed: ${fact1} !== ${fact2}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
