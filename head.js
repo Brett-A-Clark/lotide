b32e02a5469f878ac7354d8aae9d960dcbfbb38d
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-26 22:05:22 +0000

    Further improvements to file

--- head.js
+++ head.js
@@ -1,15 +1,4 @@
-const assertEqual = require('./assertEqual');
-
-assertEqual = function(actual, expected) {
-  const fact1 = actual;
-  const fact2 = expected;
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${fact1} === ${fact2}`);
-  } else {
-    console.log(`Assertion Failed: ${fact1} !== ${fact2}`);
-  }
-};
-
+const assertEqual = require('./assertEqual.js');
 
 const head = function(leg) {
   return leg[0];
